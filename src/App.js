a729b8c2928723c6638375afe84cdbbbb95d709d
Mohamad EL-Chanti <m.elchanti@gmail.com>
2022-08-11 16:34:58 -0400

    displayed raw data as table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data';
+import { Table } from 'react-bootstrap';
 
-const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+const Routes = () => {
+  return (
+    <div>
+      <h2>Routes</h2>
+      <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Airline</th>
+          <th>src</th>
+          <th>dest</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.routes.map((route, index) => 
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{route.airline}</td>
+              <td>{route.src}</td>
+              <td>{route.dest}</td>
+            </tr>
+        )}
+      </tbody>
+    </Table>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="app container">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <p>
+          Welcome to the app!
+        </p>
+      </section>
+      <Routes></Routes>
+    </div>
+  )
+}
 
 export default App;
